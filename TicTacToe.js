588701a249e02246f689319e0449e249cecac4d1
Shubham Rindhe <shubhamrindhe00@gmail.com>
2019-04-25 11:39:59 +0530

    HTML<

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,11 +1,35 @@
 // Author : Shubham Rindhe
 // github : github.com/shubhamrindhe
 
+var Heuristic_Array = [
+	[     0,   -10,  -100, -1000 ],
+	[    10,     0,     0,     0 ],
+	[   100,     0,     0,     0 ],
+	[  1000,     0,     0,     0 ]
+];	
+
+function evaluatePosition(board,player) {
+	var opponent = (player == 'X') ? 'O' : 'X', piece;
+	var players, others, t = 0, i, j; 
+	for (i = 0; i < 8; i++)  {
+		players = others = 0;
+		for (j = 0; j < 3; j++)  {
+			piece = board[TicTacToe.winning_combinations()[i][j]];
+			if (piece == player)
+				players++;
+			else if (piece == opponent)
+				others++;
+		}
+		t += Heuristic_Array[players][others];
+	}
+	return t;
+}
+
 
 class TicTacToe {
 
 	constructor(){
-		this.board = Array.from(Array(9).keys());
+		this.board = Array.from( Array(9).keys() );
 	}
 	
 	static winning_combinations(){
@@ -162,11 +186,162 @@ class TicTacToe {
 }
 
 
+function empty_cells(list){
+	return list.filter( s => typeof s == 'number' );
+}
 	
+function checkTie(){
+	if(empty_cells(grid).length==0)
+		return true;
+	else
+		return false;	
+}
+	
+function checkWin(board,player){
+	let plays = board.reduce( (a,e,i) => ( (e === player) ? a.concat(i) : a ) , [] );			
+	let gamewin = null;
+
+	for(let [index,combo] of TicTacToe.winning_combinations().entries()){
+		if( combo.every(elem=>plays.indexOf(elem)>-1) ){
+			gamewin = {
+				index : index,
+				player : player
+			};
+			break;
+		}	
+	}
+	return gamewin;
+}	
+
+	
+function minimax(board,player,mark){		
+	if(checkWin(board,mark.rival))
+		return {score:-1};
+	if(checkWin(board,mark.player))
+		return {score: 1};
+	if(checkTie())
+		return {score: 0};	
+
+	var bestscore = ( player ? -Infinity : Infinity ) ;
+	var alpha_move;	
+	/*
+	empty_cells(board).forEach(function (cell,index) {
+		var move = {};
+		move.index = board[cell];
+		board[cell] = player ? mark.player : mark.rival ;
+		if(player){
+			var result = minimax(board,false,mark);
+			move.score = result.score;
+			if(move.score > bestscore){
+				bestscore = move.score;
+				alpha_move = move;
+			}
+		}else{
+			var result = minimax(board,true,mark);
+			move.score = result.score;
+			if(move.score < bestscore){
+				bestscore = move.score;
+				alpha_move = move;
+			}
+		}
+		board[cell] = move.index;
+	});
+	*/
+
+	if(player){
+		empty_cells(board).forEach(function (cell,index) {
+			var move = {};
+			//move.index = board[cell];
+			move.index = cell;
+			board[cell] = mark.player;
+			var result = minimax(board,false,mark);
+			move.score = result.score;
+			board[cell] = cell;
+			if(move.score > bestscore){
+				bestscore = move.score;
+				alpha_move = move;
+			}
+			
+		});
+	}else{
+		empty_cells(board).forEach(function (cell,index) {
+			var move = {};
+			//move.index = board[cell];
+			move.index = cell;
+			board[cell] = mark.rival;			
+			var result = minimax(board,true,mark);
+			move.score = result.score;
+			board[cell] = cell;
+			if(move.score < bestscore){
+				bestscore = move.score;
+				alpha_move = move;
+			}
+			
+		});
+	}
+
+	
+	return alpha_move;	
+}
+
+
+function minmax(board,player,alpha=-Infinity,beta=Infinity,level=2){
+	var avail_spots = empty_cells(board);
+	if(checkWin(board,hooman))
+		//return {score:evaluatePosition(board,player)};
+		return {score:-1000};
+	if(checkWin(board,puter))
+		//return {score:evaluatePosition(board,player)};
+		return {score: 1000};
+	if(avail_spots.length==0)
+		//return {score:evaluatePosition(board,player)};	
+		return {score:0};	
+	if (level == 0) 
+		//return {score:evaluatePosition(board,player)}; 	
+		return {score:evaluatePosition(board,player)};
+	/*
+	if (avail_spots.length == 0) 
+		return {score:evaluatePosition(board,player)}; 
+	*/
+	var bestscore = (player==puter ? -Infinity : Infinity) ;
+	var alpha_move;
+	for(var i=0;i<avail_spots.length;++i){
+		var move = {};
+		move.index = board[avail_spots[i]];
+		board[avail_spots[i]] = player;
+		if(player==puter){
+			var result = minmax(board,hooman,alpha,beta,level-1);
+			move.score = result.score;
+			if(move.score > bestscore){
+				bestscore = move.score;
+				alpha_move = move;
+			}
+			alpha = Math.max(alpha,bestscore);
+			if(beta<=alpha){
+				board[avail_spots[i]] = move.index;
+				break;
+			}
+		}else{
+			var result = minmax(board,puter,alpha,beta,level-1);
+			move.score = result.score;
+			if(move.score < bestscore){
+				bestscore = move.score;
+				alpha_move = move;
+			}
+			beta = Math.min(alpha,bestscore);
+			if(beta<=alpha){
+				board[avail_spots[i]] = move.index;
+				break;
+			}
+		}
+		board[avail_spots[i]] = move.index;
+	}
+	return alpha_move;
+}
 
 
 
-
+/*
 
 var t = new TicTacToe();
 t.board[0] = 'X';
@@ -181,3 +356,5 @@ console.log(t.check_win('X'));
 console.log(t.minimax(true,{player:'O',rival:'X'}));
 console.log(TicTacToe.minimax(t,true,{player:'O',rival:'X'}));
 //console.log(minimax(t.board,true,{player:'O',rival:'X'}));
+
+*/
