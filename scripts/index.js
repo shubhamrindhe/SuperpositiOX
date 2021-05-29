20eee93a9530f7ceabaa06e6b878dc409a439594
shubhamrindhe <rindheshubham@gmail.com>
2021-05-29 12:20:02 +0530

    O-anim

--- scripts/index.js
+++ scripts/index.js
@@ -5,345 +5,357 @@ var cells = document.getElementsByClassName('cell');
 var X = '<svg width="80" height="80" class="">\
 		<line x1="20" y1="20" x2="60" y2="60" style="stroke:blue;stroke-width:5" stroke-linecap="round"/>\
 		<line x1="20" y1="60" x2="60" y2="20" style="stroke:blue;stroke-width:5" stroke-linecap="round"/>\
-		</svg>';	
+		</svg>';
 var O = '<svg width="80" height="80" class="">\
 		<circle cx="40" cy="40" r="25" stroke="red" fill="none" stroke-width="5" />\
 		</svg>';
-	
+
 var hooman = 'X';
 var puter = 'O';
 
 var dim = 3;
-	
+
 var size = 9;
 var Tic_Tac_Toe;
 var grid;
 var turn;
 var winning_combos_cords = [
-	{x1:0.05,y1:1/6,x2:0.95,y2:1/6},
-	{x1:0.05,y1:3/6,x2:0.95,y2:3/6},
-	{x1:0.05,y1:5/6,x2:0.95,y2:5/6},
+	{ x1: 0.05, y1: 1 / 6, x2: 0.95, y2: 1 / 6 },
+	{ x1: 0.05, y1: 3 / 6, x2: 0.95, y2: 3 / 6 },
+	{ x1: 0.05, y1: 5 / 6, x2: 0.95, y2: 5 / 6 },
 
-	{x1:1/6,y1:0.05,x2:1/6,y2:0.95},
-	{x1:3/6,y1:0.05,x2:3/6,y2:0.95},
-	{x1:5/6,y1:0.05,x2:5/6,y2:0.95},
+	{ x1: 1 / 6, y1: 0.05, x2: 1 / 6, y2: 0.95 },
+	{ x1: 3 / 6, y1: 0.05, x2: 3 / 6, y2: 0.95 },
+	{ x1: 5 / 6, y1: 0.05, x2: 5 / 6, y2: 0.95 },
 
-	{x1:0.05,y1:0.05,x2:0.95,y2:0.95},
-	{x1:0.95,y1:0.05,x2:0.05,y2:0.95}
+	{ x1: 0.05, y1: 0.05, x2: 0.95, y2: 0.95 },
+	{ x1: 0.95, y1: 0.05, x2: 0.05, y2: 0.95 }
 ];
 var grid_center_cords = [
-	{x:1/6,y:1/6},
-	{x:3/6,y:1/6},
-	{x:5/6,y:1/6},
+	{ x: 1 / 6, y: 1 / 6 },
+	{ x: 3 / 6, y: 1 / 6 },
+	{ x: 5 / 6, y: 1 / 6 },
 
-	{x:1/6,y:3/6},
-	{x:3/6,y:3/6},
-	{x:5/6,y:3/6},
-	
-	{x:1/6,y:5/6},
-	{x:3/6,y:5/6},
-	{x:5/6,y:5/6}
+	{ x: 1 / 6, y: 3 / 6 },
+	{ x: 3 / 6, y: 3 / 6 },
+	{ x: 5 / 6, y: 3 / 6 },
+
+	{ x: 1 / 6, y: 5 / 6 },
+	{ x: 3 / 6, y: 5 / 6 },
+	{ x: 5 / 6, y: 5 / 6 }
 ];
 
-function index_array(n){
+function index_array(n) {
 	return Array.from(Array(n).keys());
 }
 
-function beginElements(list){
-	list.forEach(function(e,i,l){
+function beginElements(list) {
+	list.forEach(function (e, i, l) {
 		e.beginElement();
 	});
 }
-function beginElementsById(list){
-	list.forEach(function(e,i,l){
+function beginElementsById(list) {
+	list.forEach(function (e, i, l) {
 		document.getElementById(e).beginElement();
 	});
 }
 
-function create_line(x1,y1,x2,y2,color,lineWidth,animations){
+function create_line(x1, y1, x2, y2, color, lineWidth, animations) {
 	animations_list = [];
-	
-	var line = document.createElementNS('http://www.w3.org/2000/svg','line');
-	line.setAttribute("x1",x1);
-	line.setAttribute("y1",y1);
-	line.setAttribute("x2",x2);
-	line.setAttribute("y2",y2);
-	line.setAttribute("stroke",color);
-	line.setAttribute("stroke-width",lineWidth);
-	line.setAttribute("stroke-linecap","round");
-	
-	animations.forEach(function(e,i,l){
-		var animation = document.createElementNS('http://www.w3.org/2000/svg','animate');
-		for(anim_attr in e){
-			animation.setAttribute(anim_attr,e[anim_attr]);
+
+	var line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+	line.setAttribute("x1", x1);
+	line.setAttribute("y1", y1);
+	line.setAttribute("x2", x2);
+	line.setAttribute("y2", y2);
+	line.setAttribute("stroke", color);
+	line.setAttribute("stroke-width", lineWidth);
+	line.setAttribute("stroke-linecap", "round");
+
+	animations.forEach(function (e, i, l) {
+		var animation = document.createElementNS('http://www.w3.org/2000/svg', 'animate');
+		for (anim_attr in e) {
+			animation.setAttribute(anim_attr, e[anim_attr]);
 		}
 		line.appendChild(animation);
 		animations_list.push(animation);
 	});
-	
-	return {line,animations_list};
+
+	return { line, animations_list };
 }
 
-function init_grid(){
-	
+function init_grid() {
+
 	var rect = Tic_Tac_Toe.getBoundingClientRect();
 	var W = rect.width;
-	var w = W/dim;
-	var l=4;
+	var w = W / dim;
+	var l = 4;
 	var color = 'black'
 	var _ = 4;
 	var anim_list = [];
-	
-	var line_h1_opt = create_line(W/2,w,W/2,w,color,l,[
+
+	var line_h1_opt = create_line(W / 2, w, W / 2, w, color, l, [
 		{
-			attributeName : "x1",
-			from : W/2,
-			to : 0+_,
-			dur : "0.3s",
-			begin : "0s",
-			fill : "freeze"
-		},{
-			attributeName : "x2",
-			from : W/2,
-			to : W-_,
-			dur : "0.3s",
-			begin : "0s",
-			fill : "freeze"
+			attributeName: "x1",
+			from: W / 2,
+			to: 0 + _,
+			dur: "0.3s",
+			begin: "0s",
+			fill: "freeze"
+		}, {
+			attributeName: "x2",
+			from: W / 2,
+			to: W - _,
+			dur: "0.3s",
+			begin: "0s",
+			fill: "freeze"
 		}
 	]);
-	document.getElementById('XO').appendChild( line_h1_opt.line );
+	document.getElementById('XO').appendChild(line_h1_opt.line);
 	anim_list = anim_list.concat(line_h1_opt.animations_list);
-	
-	var line_h2_opt = create_line(W/2,2*w,W/2,2*w,color,l,[
+
+	var line_h2_opt = create_line(W / 2, 2 * w, W / 2, 2 * w, color, l, [
 		{
-			attributeName : "x1",
-			from : W/2,
-			to : 0+_,
-			dur : "0.3s",
-			begin : "0s",
-			fill : "freeze"
-		},{
-			attributeName : "x2",
-			from : W/2,
-			to : W-_,
-			dur : "0.3s",
-			begin : "0s",
-			fill : "freeze"
+			attributeName: "x1",
+			from: W / 2,
+			to: 0 + _,
+			dur: "0.3s",
+			begin: "0s",
+			fill: "freeze"
+		}, {
+			attributeName: "x2",
+			from: W / 2,
+			to: W - _,
+			dur: "0.3s",
+			begin: "0s",
+			fill: "freeze"
 		}
 	]);
-	document.getElementById('XO').appendChild( line_h2_opt.line );
+	document.getElementById('XO').appendChild(line_h2_opt.line);
 	anim_list = anim_list.concat(line_h2_opt.animations_list);
-	
-	var line_v1_opt = create_line(w,W/2,w,W/2,color,l,[
+
+	var line_v1_opt = create_line(w, W / 2, w, W / 2, color, l, [
 		{
-			attributeName : "y1",
-			from : W/2,
-			to : 0+_,
-			dur : "0.3s",
-			begin : "0s",
-			fill : "freeze"
-		},{
-			attributeName : "y2",
-			from : W/2,
-			to : W-_,
-			dur : "0.3s",
-			begin : "0s",
-			fill : "freeze"
+			attributeName: "y1",
+			from: W / 2,
+			to: 0 + _,
+			dur: "0.3s",
+			begin: "0s",
+			fill: "freeze"
+		}, {
+			attributeName: "y2",
+			from: W / 2,
+			to: W - _,
+			dur: "0.3s",
+			begin: "0s",
+			fill: "freeze"
 		}
 	]);
-	document.getElementById('XO').appendChild( line_v1_opt.line );
+	document.getElementById('XO').appendChild(line_v1_opt.line);
 	anim_list = anim_list.concat(line_v1_opt.animations_list);
-	
-	var line_v2_opt = create_line(2*w,W/2,2*w,W/2,color,l,[
+
+	var line_v2_opt = create_line(2 * w, W / 2, 2 * w, W / 2, color, l, [
 		{
-			attributeName : "y1",
-			from : W/2,
-			to : 0+_,
-			dur : "0.3s",
-			begin : "0s",
-			fill : "freeze"
-		},{
-			attributeName : "y2",
-			from : W/2,
-			to : W-_,
-			dur : "0.3s",
-			begin : "0s",
-			fill : "freeze"
+			attributeName: "y1",
+			from: W / 2,
+			to: 0 + _,
+			dur: "0.3s",
+			begin: "0s",
+			fill: "freeze"
+		}, {
+			attributeName: "y2",
+			from: W / 2,
+			to: W - _,
+			dur: "0.3s",
+			begin: "0s",
+			fill: "freeze"
 		}
 	]);
-	document.getElementById('XO').appendChild( line_v2_opt.line );
+	document.getElementById('XO').appendChild(line_v2_opt.line);
 	anim_list = anim_list.concat(line_v2_opt.animations_list);
-	
+
 	beginElements(anim_list);
 }
 
 
 
-	
-function create_svg(i){
+
+function create_svg(i) {
 	var win = winning_combos_cords[i];
 	var w = document.getElementById('winning_move').getBoundingClientRect().width;
-	
-	var line = create_line( win.x1*w , win.y1*w , win.x1*w , win.y1*w , "rgba(0,0,0,1)" ,10,[
+
+	var line = create_line(win.x1 * w, win.y1 * w, win.x1 * w, win.y1 * w, "rgba(0,0,0,1)", 10, [
 		{
-			attributeName : "x2",
-			from : win.x1*w,
-			to : win.x2*w,
-			dur : "0.3s",
-			begin : "0s",
-			fill : "freeze"
-		},{
-			attributeName : "y2",
-			from : win.y1*w,
-			to : win.y2*w,
-			dur : "0.3s",
-			begin : "0s",
-			fill : "freeze"
+			attributeName: "x2",
+			from: win.x1 * w,
+			to: win.x2 * w,
+			dur: "0.3s",
+			begin: "0s",
+			fill: "freeze"
+		}, {
+			attributeName: "y2",
+			from: win.y1 * w,
+			to: win.y2 * w,
+			dur: "0.3s",
+			begin: "0s",
+			fill: "freeze"
 		}
 	]);
-	
-	return {line:line.line,animate_tags:line.animations_list};
+
+	return { line: line.line, animate_tags: line.animations_list };
 }
 
-function create_o(i){
+function create_o(i) {
 	var c = grid_center_cords[i];
 	var w = document.getElementById('XO').getBoundingClientRect().width;
-	
-	var circle = document.createElementNS('http://www.w3.org/2000/svg','circle');
-	circle.setAttribute("cx",c.x*w);
-	circle.setAttribute("cy",c.y*w);
-	circle.setAttribute("r",w/(1.5*size));
-	circle.setAttribute("stroke","blue");
-	circle.setAttribute("fill","rgba(255,255,255,0)");
-	circle.setAttribute("stroke-width",stroke_width);
-	
-	var animate = document.createElementNS('http://www.w3.org/2000/svg','animate');
-	animate.setAttribute("attributeName","r");
-	animate.setAttribute("from",0);
-	animate.setAttribute("to",w/(1.5*size));
-	animate.setAttribute("dur","0.2s");
-	animate.setAttribute("begin","0s");
-	animate.setAttribute("fill","freeze");
-	
+
+	var circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+	circle.setAttribute("cx", c.x * w);
+	circle.setAttribute("cy", c.y * w);
+	circle.setAttribute("r", w / (1.5 * size));
+	circle.setAttribute("stroke", "blue");
+	circle.setAttribute("fill", "rgba(255,255,255,0)");
+	circle.setAttribute("stroke-width", stroke_width);
+
+	/*
+	var animate = document.createElementNS('http://www.w3.org/2000/svg', 'animate');
+	animate.setAttribute("attributeName", "r");
+	animate.setAttribute("from", 0);
+	animate.setAttribute("to", w / (1.5 * size));
+	animate.setAttribute("dur", "0.2s");
+	animate.setAttribute("begin", "0s");
+	animate.setAttribute("fill", "freeze");
+	*/
+	let l = circle.getTotalLength();
+	circle.setAttribute("stroke-dasharray", l);
+
+	var animate = document.createElementNS('http://www.w3.org/2000/svg', 'animate');
+	animate.setAttribute("attributeName", "stroke-dashoffset");
+	animate.setAttribute("from", l);
+	animate.setAttribute("to", 0);
+	animate.setAttribute("dur", "0.3s");
+	animate.setAttribute("begin", "0s");
+	animate.setAttribute("fill", "freeze");
+
 	circle.appendChild(animate)
-	
-	return {circle:circle,animate_tags:[animate]};
+
+	return { circle: circle, animate_tags: [animate] };
 }
 
-function create_x(i){
+function create_x(i) {
 	var c = grid_center_cords[i];
 	var w = document.getElementById('XO').getBoundingClientRect().width;
-	var s = w/(2*size);
+	var s = w / (2 * size);
 	var anim_list = [];
-	
-	var line1 = create_line( c.x*w-s , c.y*w-s , c.x*w-s , c.y*w-s , "red" , stroke_width ,[
+
+	var line1 = create_line(c.x * w - s, c.y * w - s, c.x * w - s, c.y * w - s, "red", stroke_width, [
 		{
-			attributeName : "x2",
-			from : c.x*w-s,
-			to : c.x*w+s,
-			dur : "0.3s",
-			begin : "0s",
-			fill : "freeze"
-		},{
-			attributeName : "y2",
-			from : c.y*w-s,
-			to : c.y*w+s,
-			dur : "0.3s",
-			begin : "0s",
-			fill : "freeze"
+			attributeName: "x2",
+			from: c.x * w - s,
+			to: c.x * w + s,
+			dur: "0.3s",
+			begin: "0s",
+			fill: "freeze"
+		}, {
+			attributeName: "y2",
+			from: c.y * w - s,
+			to: c.y * w + s,
+			dur: "0.3s",
+			begin: "0s",
+			fill: "freeze"
 		}
 	]);
-	
-	
+
+
 	anim_list = anim_list.concat(line1.animations_list);
-	
-	var line2 = create_line( c.x*w-s , c.y*w+s , c.x*w-s , c.y*w+s , "red" , stroke_width ,[
+
+	var line2 = create_line(c.x * w - s, c.y * w + s, c.x * w - s, c.y * w + s, "red", stroke_width, [
 		{
-			attributeName : "x2",
-			from : c.x*w-s,
-			to : c.x*w+s,
-			dur : "0.3s",
-			begin : "0s",
-			fill : "freeze"
-		},{
-			attributeName : "y2",
-			from : c.y*w+s,
-			to : c.y*w-s,
-			dur : "0.3s",
-			begin : "0s",
-			fill : "freeze"
+			attributeName: "x2",
+			from: c.x * w - s,
+			to: c.x * w + s,
+			dur: "0.3s",
+			begin: "0s",
+			fill: "freeze"
+		}, {
+			attributeName: "y2",
+			from: c.y * w + s,
+			to: c.y * w - s,
+			dur: "0.3s",
+			begin: "0s",
+			fill: "freeze"
 		}
 	]);
-	
+
 	anim_list = anim_list.concat(line2.animations_list);
-	
-	return {line1:line1.line,line2:line2.line,animate_tags:anim_list};
-	
-}	
-	
-	
 
-	
+	return { line1: line1.line, line2: line2.line, animate_tags: anim_list };
 
-	
-function init(){
+}
+
+
+
+
+
+
+function init() {
 	Tic_Tac_Toe = document.getElementById('XO')
 	grid = index_array(9);
 	turn = true;
-	
+
 	init_grid();
 }
 
 
-	
-function reset(){
+
+function reset() {
 	Tic_Tac_Toe.innerHTML = '';
 	grid = index_array(9);
 	turn = true;
-	
+
 	init_grid();
 }
 
-function addListeners(){
-	Tic_Tac_Toe.setAttribute('onclick','handle_svg(event,this)');
-	
-	for(var i=0;i<cells.length;++i){
-		cells[i].setAttribute('onclick','handle_table(event,this)');
+function addListeners() {
+	Tic_Tac_Toe.setAttribute('onclick', 'handle_svg(event,this)');
+
+	for (var i = 0; i < cells.length; ++i) {
+		cells[i].setAttribute('onclick', 'handle_table(event,this)');
 		//Tic_Tac_Toe[i].setAttribute('onclick','handleTurn(event,'+i+')');
 	}
-	
+
 }
 
-function handle_svg(e,DOM){
+function handle_svg(e, DOM) {
 	var rect = DOM.getBoundingClientRect();
-	var i = Math.floor((e.x-rect.left)/ (rect.width/dim) );
-	var j = Math.floor((e.y-rect.top)/ (rect.height/dim) );
-	var id = i+dim*j;
-	
+	var i = Math.floor((e.x - rect.left) / (rect.width / dim));
+	var j = Math.floor((e.y - rect.top) / (rect.height / dim));
+	var id = i + dim * j;
+
 	handle(id);
 }
 
-function handle_table(e,DOM){
-	var id =  DOM.id;
+function handle_table(e, DOM) {
+	var id = DOM.id;
 	handle(id);
 }
-	
-function handle(id){
-	if(turn && (typeof grid[id] != 'string') /*DOM.innerHTML==''*/){
+
+function handle(id) {
+	if (turn && (typeof grid[id] != 'string') /*DOM.innerHTML==''*/) {
 		turn = false;
-		mark(id,hooman);
-			
-		var end = check_win(grid,hooman);
-		if(end == null){
-			if(!check_tie(grid)){
-				
+		mark(id, hooman);
+
+		var end = check_win(grid, hooman);
+		if (end == null) {
+			if (!check_tie(grid)) {
+
 				document.getElementById('loader').style.display = 'block';
-				
+
 				/*
 				var spot = best_spot();
 				mark(spot,'O');
 				document.getElementById('loader').style.display = 'none';
-				
+
 				end = check_win(grid,puter);
 				if(end == null){
 				}else{
@@ -351,89 +363,91 @@ function handle(id){
 					turn = false;
 				}
 				*/
-				
-				
+
+
 				best_spot().then(spot => {
-					mark(spot,'O');
-					document.getElementById('loader').style.display = 'none';
-				
-					end = check_win(grid,puter);
-					if(end == null){
+					setTimeout(() => {
+						mark(spot, 'O');
+						document.getElementById('loader').style.display = 'none';
 
-					}else{
-						endgame(end.player,end.index);
-						turn = false;
-					}
+						end = check_win(grid, puter);
+						if (end == null) {
+
+						} else {
+							endgame(end.player, end.index);
+							turn = false;
+						}
+					}, 5)
 				});
-				
-				
 
-				
-			}else{
-				endgame('-',null);
+
+
+
+			} else {
+				endgame('-', null);
 			}
 			turn = true;
-		}else{
-			endgame(end.player,end.index);
+		} else {
+			endgame(end.player, end.index);
 			turn = false;
-		}	
-	}			
+		}
+	}
 }
 
-function mark(cell_id,player){
+function mark(cell_id, player) {
 	//Tic_Tac_Toe[cell_id].innerHTML = (player=='O'?O:X);
-	if(player=='X'){
+	if (player == 'X') {
 		//document.getElementById('XO').innerHTML += create_x(cell_id);
 		var x = create_x(cell_id);
-		document.getElementById('XO').appendChild( x.line1 );
-		document.getElementById('XO').appendChild( x.line2 );
+		document.getElementById('XO').appendChild(x.line1);
+		document.getElementById('XO').appendChild(x.line2);
 		beginElements(x.animate_tags);
-	}else{
+	} else {
 		//document.getElementById('XO').innerHTML += create_o(cell_id);
 		var o = create_o(cell_id);
-		document.getElementById('XO').appendChild( o.circle );
+		document.getElementById('XO').appendChild(o.circle);
 		beginElements(o.animate_tags);
 	}
 	grid[cell_id] = player;
 }
 
-function best_spot(){
-	
+function best_spot() {
+
 	return new Promise(resolve => {
-		resolve(minmax_random(grid,puter,{protagonist : puter , rival : hooman},0.3).index);
+		resolve(minmax_random(grid, puter, { protagonist: puter, rival: hooman }, 0.3).index);
 	});
-	
+
 	//return minmax_random(grid,puter,{protagonist : puter , rival : hooman},0.3).index;
 }
-		
-function endgame(player,combo){
-	if(player == puter){
-		document.getElementById('result').innerHTML = 'You Lost!';	
-	}else if(player == hooman){
+
+function endgame(player, combo) {
+	if (player == puter) {
+		document.getElementById('result').innerHTML = 'You Lost!';
+	} else if (player == hooman) {
 		document.getElementById('result').innerHTML = 'You Won!';
-	}else{
+	} else {
 		document.getElementById('result').innerHTML = 'Its a Tie!';
-	}	
-		
-	
+	}
+
+
 	document.getElementById('result').className = 'active';
 	document.getElementById('start_btn').className = 'active';
-		
-	if(combo!=null){
-		for(var i=0;i<TicTacToe_winning_combinations[combo].length;++i){
-		
+
+	if (combo != null) {
+		for (var i = 0; i < TicTacToe_winning_combinations[combo].length; ++i) {
+
 		}
 		document.getElementById('winning_move').style.display = 'block';
-		//document.getElementById('winning_move').innerHTML = create_svg(combo);	
-		
+		//document.getElementById('winning_move').innerHTML = create_svg(combo);
+
 		var l = create_svg(combo)
-		document.getElementById('winning_move').appendChild( l.line );
+		document.getElementById('winning_move').appendChild(l.line);
 		//l.animate_tags[0].beginElement();
 		beginElements(l.animate_tags);
-	}	
+	}
 }
 
-function restart(){
+function restart() {
 	document.getElementById('result').className = 'not-active';
 	document.getElementById('start_btn').className = 'not-active';
 	document.getElementById('winning_move').style.display = 'none';
@@ -442,7 +456,7 @@ function restart(){
 	reset();
 }
 
-window.onload = function(){
+window.onload = function () {
 	init();
 	addListeners();
 }
--- scripts/minmax.js
+++ scripts/minmax.js
@@ -1,16 +1,16 @@
 var Heuristic_Array = [
-	[     0,   -10,  -100, -1000 ],
-	[    10,     0,     0,     0 ],
-	[   100,     0,     0,     0 ],
-	[  1000,     0,     0,     0 ]
-];	
+	[0, -10, -100, -1000],
+	[10, 0, 0, 0],
+	[100, 0, 0, 0],
+	[1000, 0, 0, 0]
+];
 
-function evaluatePosition(board,player) {
+function evaluatePosition(board, player) {
 	var opponent = (player == 'X') ? 'O' : 'X', piece;
-	var players, others, t = 0, i, j; 
-	for (i = 0; i < 8; i++)  {
+	var players, others, t = 0, i, j;
+	for (i = 0; i < 8; i++) {
 		players = others = 0;
-		for (j = 0; j < 3; j++)  {
+		for (j = 0; j < 3; j++) {
 			piece = board[TicTacToe.winning_combinations()[i][j]];
 			if (piece == player)
 				players++;
@@ -23,72 +23,72 @@ function evaluatePosition(board,player) {
 }
 
 var TicTacToe_winning_combinations = [
-	[0,1,2],
-	[3,4,5],
-	[6,7,8],
-			
-	[0,3,6],
-	[1,4,7],
-	[2,5,8],	
-		
-	[0,4,8],
-	[2,4,6]
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+
+	[0, 4, 8],
+	[2, 4, 6]
 ];
 
-function empty_cells(list){
-	return list.filter( s => typeof s == 'number' );
+function empty_cells(list) {
+	return list.filter(s => typeof s == 'number');
 }
-	
-function check_tie(_){
-	if((empty_cells(_).length)==0)
+
+function check_tie(_) {
+	if ((empty_cells(_).length) == 0)
 		return true;
 	else
-		return false;	
+		return false;
 }
-	
-function check_win(_,player){
-	let plays = _.reduce( (a,e,i) => ( (e === player) ? a.concat(i) : a ) , [] );			
+
+function check_win(_, player) {
+	let plays = _.reduce((a, e, i) => ((e === player) ? a.concat(i) : a), []);
 	let gamewin = null;
 
-	for(let [index,combo] of TicTacToe_winning_combinations.entries()){
-		if( combo.every(elem=>plays.indexOf(elem)>-1) ){
+	for (let [index, combo] of TicTacToe_winning_combinations.entries()) {
+		if (combo.every(elem => plays.indexOf(elem) > -1)) {
 			gamewin = {
-				index : index,
-				player : player
+				index: index,
+				player: player
 			};
 			break;
-		}	
+		}
 	}
 	return gamewin;
 }
 
-function minmax(state,player,mark){
+function minmax(state, player, mark) {
 	var _ = state;//state.slice();
-	
-	if(check_win(_,mark.protagonist))
-		return {score: 1};
-	if(check_win(_,mark.rival))
-		return {score:-1};
-	if(check_tie(_))
-		return {score: 0};	
 
-	var bestscore = ( player==mark.protagonist ? -Infinity : Infinity ) ;
-	var alpha_move;	
-	empty_cells(_).forEach(function (cell,index,list) {
+	if (check_win(_, mark.protagonist))
+		return { score: 1 };
+	if (check_win(_, mark.rival))
+		return { score: -1 };
+	if (check_tie(_))
+		return { score: 0 };
+
+	var bestscore = (player == mark.protagonist ? -Infinity : Infinity);
+	var alpha_move;
+	empty_cells(_).forEach(function (cell, index, list) {
 		var move = {};
 		move.index = _[cell];
-		_[cell] = player==mark.protagonist ? mark.protagonist : mark.rival ;
-		if( player == mark.protagonist){
-			var result = minmax(_,mark.rival,mark);
+		_[cell] = player == mark.protagonist ? mark.protagonist : mark.rival;
+		if (player == mark.protagonist) {
+			var result = minmax(_, mark.rival, mark);
 			move.score = result.score;
-			if(move.score > bestscore){
+			if (move.score > bestscore) {
 				bestscore = move.score;
 				alpha_move = move;
 			}
-		}else{
-			var result = minmax(_,mark.protagonist,mark);
+		} else {
+			var result = minmax(_, mark.protagonist, mark);
 			move.score = result.score;
-			if(move.score < bestscore){
+			if (move.score < bestscore) {
 				bestscore = move.score;
 				alpha_move = move;
 			}
@@ -98,37 +98,37 @@ function minmax(state,player,mark){
 	return alpha_move;
 }
 
-function minmax_random(state,player,mark,prob=0.5){
+function minmax_random(state, player, mark, prob = 0.5) {
 	var _ = state;//state.slice();
-	
-	if(check_win(_,mark.protagonist))
-		return {score: 1};
-	if(check_win(_,mark.rival))
-		return {score:-1};
-	if(check_tie(_))
-		return {score: 0};	
 
-	var bestscore = ( player==mark.protagonist ? -Infinity : Infinity ) ;
-	var alpha_move;	
-	empty_cells(_).forEach(function (cell,index,list) {
+	if (check_win(_, mark.protagonist))
+		return { score: 1 };
+	if (check_win(_, mark.rival))
+		return { score: -1 };
+	if (check_tie(_))
+		return { score: 0 };
+
+	var bestscore = (player == mark.protagonist ? -Infinity : Infinity);
+	var alpha_move;
+	empty_cells(_).forEach(function (cell, index, list) {
 		var move = {};
 		move.index = _[cell];
-		_[cell] = player==mark.protagonist ? mark.protagonist : mark.rival ;
-		if( player == mark.protagonist){
-			var result = minmax_random(_,mark.rival,mark,prob);
+		_[cell] = player == mark.protagonist ? mark.protagonist : mark.rival;
+		if (player == mark.protagonist) {
+			var result = minmax_random(_, mark.rival, mark, prob);
 			move.score = result.score;
-			if(move.score == bestscore && Math.random()<prob){
+			if (move.score == bestscore && Math.random() < prob) {
 				bestscore = move.score;
 				alpha_move = move;
 			}
-			if(move.score > bestscore){
+			if (move.score > bestscore) {
 				bestscore = move.score;
 				alpha_move = move;
 			}
-		}else{
-			var result = minmax_random(_,mark.protagonist,mark,prob);
+		} else {
+			var result = minmax_random(_, mark.protagonist, mark, prob);
 			move.score = result.score;
-			if(move.score < bestscore){
+			if (move.score < bestscore) {
 				bestscore = move.score;
 				alpha_move = move;
 			}
